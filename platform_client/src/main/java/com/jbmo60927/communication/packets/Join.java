346b71bfba01ccbcb8af3574e52d22b9e0297254
Jean-Baptiste Moufle <jb.moufle@gmail.com>
2022-12-31 11:39:53 +0100

    app for send and receive thread

---
+++ platform_client/src/main/java/com/jbmo60927/communication/packets/Join.java
@@ -0,0 +1,32 @@
+package com.jbmo60927.communication.packets;
+
+import static com.jbmo60927.utilz.HelpsMethods.stringToBytes;
+
+import com.jbmo60927.App;
+import com.jbmo60927.communication.Parameter;
+import com.jbmo60927.communication.types.StringTypeList;
+
+public class Join extends Packet {
+
+    protected static final StringTypeList TYPES = new StringTypeList(new String[] {
+        "name", "version"
+    });
+
+    private final App app;
+
+    protected Join(Parameter[] parameters, App app) {
+        super(Packet.TYPES.findType("Join"), setParameters(app));
+        this.app = app;
+    }
+
+    private static final Parameter[] setParameters(App app) {
+        return new Parameter[] {
+            new Parameter(Welcome.TYPES.findType("name"), stringToBytes(app.getConnect().getGameLinkThread().getName())),
+            new Parameter(Welcome.TYPES.findType("version"), stringToBytes(App.VERSION))
+        };
+    }
+
+    @Override
+    public void execute() {
+    }
+}
